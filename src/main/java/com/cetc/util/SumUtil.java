dbb9dc1c2e7fa6183da2351fb1dc34e86869b986
lilin <lilin409546297@*>
2019-09-17 16:29:41 +0800

    init

---
+++ src/main/java/com/cetc/util/SumUtil.java
@@ -0,0 +1,27 @@
+package com.cetc.util;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+import java.io.File;
+
+public class SumUtil {
+
+    public interface Sum extends Library {
+
+        int sum(int x, int y);
+
+        Sum INSTANCE = (Sum) Native.loadLibrary("sum", Sum.class);
+    }
+
+    static {
+        File file = new File("src/main/resources/lib/sum.dll");
+        System.load(file.getAbsolutePath());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("***************************");
+        System.out.println(Sum.INSTANCE.sum(1,2));
+        System.out.println("***************************");
+    }
+}
